c0a122fabd0cda73e8fa139b73fb8b049b1d6097
Ayush Trivedi <ayushtrivedi890@gmail.com>
2025-04-21 12:35:27 +0530

    cicd pushed

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
 resource "aws_instance" "strapi" {
   ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 in us-east-1
   instance_type = "t2.micro"
--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -9,5 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
